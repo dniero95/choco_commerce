b2304c54a28a81eab6bf8ef07563350aef2de2ff
Dario Niero <dario.niero@enginius.com>
2023-07-16 15:39:54 +0200

    init spring project

---
+++ choco-commerce/src/main/java/its/darioniero/chococommerce/ChocoCommerceApplication.java
@@ -0,0 +1,13 @@
+package its.darioniero.chococommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChocoCommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChocoCommerceApplication.class, args);
+	}
+
+}
---
+++ choco-commerce/src/test/java/its/darioniero/chococommerce/ChocoCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package its.darioniero.chococommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChocoCommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
